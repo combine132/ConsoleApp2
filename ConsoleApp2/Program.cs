93bfb85effadd747f62c9182b58806e877674a54
Combine132 <simonmadsen132@gmail.com>
2020-11-10 09:51:18 +0100

    Fixed FormatException

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp2
             bool loop = true;
             do {
                 Console.Title = $"The Game - Current Wins: {Games.winsTotal}";
-                Console.WriteLine($"Welcome to the GAME\nSelect a game below\n--------------------------\n" +
+                Console.WriteLine($"Welcome to the GAME\nSelect an item below\n--------------------------\n" +
                     $"  - 1. Guess a number\n" +
                     $"  - 2. Rock Paper Scissors\n" +
                     $"  - 3. Roll The Dice\n" +
@@ -20,50 +20,57 @@ namespace ConsoleApp2
                     $"  - 8. \n" +
                     $"  - 9. Heads or Tails\n" +
                     $"  - 0. Exit");
-                int input = Convert.ToInt32(Console.ReadLine());
-                switch (input) {
-                    case 1:
-                        Console.Clear();
-                        Games.GuessingGame();
-                        break;
-                    case 2:
-                        Console.Clear();
-                        Games.RockPaperScissor();
-                        break;
-                    case 3:
-                        Console.Clear();
-                        Games.RollTheDice();
-                        break;
-                    case 4:
-                        Console.Clear();
-                        Games.MiniDND();
-                        break;
-                    case 5:
-                        Console.Clear();
-                        Games.The200IQSuperQuiz();
-                        break;
-                    case 6:
-                        Console.Clear();
-                        Games.MathGame();
-                        break;
-                    case 7:
-                        Console.Clear();
-                        Games.Game7();
-                        break;
-                    case 8:
-                        Console.Clear();
-                        Games.Game8();
-                        break;
-                    case 9:
-                        Games.CoinFlip();
-                        Console.Clear();
-                        break;
-                    case 0:
-                        loop = false;
-                        break;
-                    default:
-                        Console.Clear();
-                        break;
+
+                try {
+                    int input = Convert.ToInt32(Console.ReadLine());
+                    switch (input) {
+                        case 1:
+                            Console.Clear();
+                            Games.GuessingGame();
+                            break;
+                        case 2:
+                            Console.Clear();
+                            Games.RockPaperScissor();
+                            break;
+                        case 3:
+                            Console.Clear();
+                            Games.RollTheDice();
+                            break;
+                        case 4:
+                            Console.Clear();
+                            Games.MiniDND();
+                            break;
+                        case 5:
+                            Console.Clear();
+                            Games.The200IQSuperQuiz();
+                            break;
+                        case 6:
+                            Console.Clear();
+                            Games.MathGame();
+                            break;
+                        case 7:
+                            Console.Clear();
+                            Games.Game7();
+                            break;
+                        case 8:
+                            Console.Clear();
+                            Games.Game8();
+                            break;
+                        case 9:
+                            Games.CoinFlip();
+                            Console.Clear();
+                            break;
+                        case 0:
+                            loop = false;
+                            break;
+                        default:
+                            Console.Clear();
+                            break;
+                    }
+                }
+                catch (FormatException) {
+                    Console.Clear();
+                    Console.WriteLine("Input has to be a number.\n");
                 }
             } while (loop);
         }
